975bd275ba1c440725a34c8572121e903339e61b
Cristina Jung <cristijung@gmail.com>
2024-10-15 10:24:35 -0300

    criação do header e refatoração e correção de warnings

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # App Flutterbank
 
 - Trabalhando com estilos e widgets
+____________________________________________
+
+## Observações importantes do código:
+### Refatoração e correção de código
+- Remoção dos warnings: O código agora está livre de warnings, tornando-o mais limpo e fácil de entender.
+- Melhoria de desempenho: O uso do const onde apropriado ajuda o Flutter a otimizar o desempenho do aplicativo, pois ele sabe que esses widgets não precisam ser reconstruídos.
+- Código mais conciso: A remoção do Container desnecessário torna o código mais conciso e fácil de ler.
--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -5,22 +5,29 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('\$1000.00'),
-                  Text('Balanço disponível'),
-                ],
-              ),
-              Icon(Icons.account_circle),
-            ]),
-      ),
+    return const Padding(
+      padding: EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  '\$1000.00',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Saldo disponível',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            Icon(
+              Icons.account_circle,
+              size: 42,
+            ),
+          ]),
     );
   }
 }
